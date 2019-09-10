d902934fad7bf8959564dad11b8c0baf5a7949c6
Petrov2g <dimsipecrov@gmail.com>
2019-09-10 12:59:20 +0200

    Data Types
    Kort uppgift

---
+++ src/DataTypes.java
@@ -0,0 +1,13 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		int r = 8;
+		double p = 3.14;
+		double summan = r * r * p;
+		System.out.println(summan);
+
+	}
+
+}
